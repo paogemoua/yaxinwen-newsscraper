9ad9ccf264575aa3a435154a94a6df79152af847
Paoge Moua <paoge.moua@gmail.com>
2019-02-08 14:39:13 -0600

    Fixing bad files

--- server.js
+++ server.js
@@ -31,65 +31,16 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
+var db = mongoose.connection;
 
-app.get("/", (req, res) => {
-
-    res.render("inbox");
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
 });
 
-app.get("/scrape", (reg, res) => {
-    axios.get("https://www.rfa.org/english/").then((response) => {
-        var $ = cheerio.load(response.data);
-        $("span.no_media").each(function(i, element) {
-            var result = {};
-            
-            result.title = $(this).text();
-            result.link = $(this).children("a").attr("href");
-            result.summary = $(this).siblings("p").text();
-
-            db.Article.create(result).then((dbArticle) => {
-
-            }).catch((err) => res.json(err));
-        });
-        res.redirect("/articles");
-    });
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
 });
 
-app.get("/articles", (req, res) => {
-    db.Article.find({}).then((dbArticle)); {
-        res.render("index", { dbArticle })
-        .catch((err) => {
-        res.json(err);
-    });
-
-app.get("/articles/:id", (req, res) => {
-    db.Article.findOne({ _id: req.params.id})
-    .populate("note").then((dbArticle) => {
-        res.json(dbArticle);
-    })
-    .catch((err) => {
-        res.json(err);
-    })
-});
-
-app.post("/article/:id", (req, res) => {
-    db.Note.create(req.body).then((dbNote) => db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true}))
-    .then((dbArticle) => {res.json(dbArticle);
-    })
-    .catch((err) => {
-        res.json(err);
-    })
-});
-
-app.delete("/articles/:id", (req, res) => {
-    db.Note.remove().then((dbNote) => {
-        res.json(dbNote);
-    })
-    .catch((err) => {
-        res.json(err);
-    })
-});
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${ PORT }! Refer to ${ PORT } for the app.`);
+app.listen(port, function() {
+    console.log("App running on port 3000!");
 });
